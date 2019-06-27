5b1a917205733cf6505e044a4d8cc400170a077d
hiko_t <yotti4415@gmail.com>
2019-06-27 14:32:35 +0000

    テストクラスにDBの生成と初期化を追加

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -1,14 +1,15 @@
+# coding: utf-8
 import os
 import flaskr
 import unittest
 import tempfile
 
 class FlaskrTestCase(unittest.TestCase):
-    
+
     # テストの準備
     def setUp(self):
         # テスト用DBを作成
-        self.db_fd, flaskr.DATABASE = tempfile.mkstemp()
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
         self.app = flaskr.app.test_client()
         flaskr.init_db()
 
@@ -18,6 +19,13 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.DATABASE)
 
-    if __name__ == '__main__':
-        unittest.main()
+    # 空DBのテスト
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
+
+
+# テスト実行
+if __name__ == '__main__':
+    unittest.main()
 
