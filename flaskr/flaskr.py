d35db3bb95c1a134674f7005d45ae2d99d65f647
hiko_t <yotti4415@gmail.com>
2019-07-02 13:45:15 +0000

    DB作成コマンドを追加

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -26,11 +26,26 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialized the database.')
 
 # アプリ起動時に実行
 @app.before_request
