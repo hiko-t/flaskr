5991ab5b00023ed291c6fe8649a49d05686b0943
hiko_t <yotti4415@gmail.com>
2019-07-06 13:15:11 +0000

    最初のテストを英語版に合わせて修正

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import os
-import flaskr
+from flaskr import flaskr
 import unittest
 import tempfile
 
@@ -10,14 +10,16 @@ class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         # テスト用DBを作成
         self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.testing = True
         self.app = flaskr.app.test_client()
-        flaskr.init_db()
+        with flaskr.app.app_context():
+            flaskr.init_db()
 
     # テストの後始末
     def tearDown(self):
         # テスト用DBを削除
         os.close(self.db_fd)
-        os.unlink(flaskr.DATABASE)
+        os.unlink(flaskr.app.config['DATABASE'])
 
     # 空DBのテスト
     def test_empty_db(self):
