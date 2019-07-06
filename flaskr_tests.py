5e626178f4239c6304f500e3ce76017b9c814a4d
hiko_t <yotti4415@gmail.com>
2019-07-06 13:23:01 +0000

    ログイン、ログアウトテストを追加

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -26,6 +26,34 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert 'No entries here so far' in rv.data
 
+    # テスト用ログイン関数
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    # テスト用ログアウト関数
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    # ログイン、ログアウトのテスト
+    def test_login_logout(self):
+        # 正常ログイン
+        rv = self.login('admin', 'default')
+        assert b'You were logged in' in rv.data
+        # 正常ログアウト
+        rv = self.logout()
+        assert b'You were logged out' in rv.data
+        # 異常ログイン　ユーザー名違い
+        rv = self.login('adminx', 'default')
+        assert b'Invalid username' in rv.data
+        # 異常ログイン　パスワード違い
+        rv = self.login('admin', 'defaultx')
+        assert b'Invalid password' in rv.data
+
+
+
 
 # テスト実行
 if __name__ == '__main__':
